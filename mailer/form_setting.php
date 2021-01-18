654371f235b4409b724eb8e8f8e367efe9a76b03
Džiugas Pečiulevičius <dziugaspeciulevicius@gmail.com>
2021-01-19 01:00:30 +0200

    contact form connected to formspree now  works

--- mailer/form_setting.php
+++ mailer/form_setting.php
@@ -1,7 +1,7 @@
 <?php
 	/*Form settings*/
 	$subj = "New message from the site 'You Site Name'"; //letter subject
-	$to = 'dziugaspeciulevicius@gmail.com'; // Enter Your E-mail
+	$to = 'beshleygame@mail.ru'; // Enter Your E-mail
 	$from = 'admin@you-site-name.com'; // Admin e-mail
 	$fromName = 'Your Company Name'; // Your company name
 	$charset = 'UTF-8';
